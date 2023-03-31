dcb3ceceb05eebc0c88578b8047e1de2abb0501b
Rye <git@itsrye.dev>
2023-03-31 21:42:57 +0200

    more refactoring

--- pronouns.ts
+++
@@ -1,2 +0,0 @@
-import * as pronounsUser from './pronounsUser'
-
---
+++ src/Language.ts
@@ -0,0 +1,9 @@
+export enum Language{
+    en = 'en',
+    de = 'de',
+    es = 'es',
+    fr = 'fr',
+    it = 'it',
+    nl = 'nl',
+    pt = 'pt'
+};
