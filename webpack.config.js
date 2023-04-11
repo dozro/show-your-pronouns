f968808a5f7eb1eef264618f1c142fd53d8c3ddd
Rye <git@itsrye.dev>
2023-04-11 14:30:19 +0200

    updated files

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: {
+      import: './src/index.ts',
+//      dependOn: 'PronounsPageUser',
+    },
+//    PronounsPageUser: './src/PronounsPageUser.ts'
+  },
+  mode: 'production',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'pronounsPage.js',
+    library: "pronouns",
+//    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
