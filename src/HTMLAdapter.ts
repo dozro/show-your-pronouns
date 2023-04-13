cc891e450b695c341acf3d9f9d449376c9e9cff3
Rye <git@itsrye.dev>
2023-04-13 19:40:17 +0200

    updated files

--- src/HTMLAdapter.ts
+++ src/HTMLAdapter.ts
@@ -1,45 +1,45 @@
-import { PronounsPageUser, newUser } from './PronounsPageUser';
+import { PronounsUser, newUser } from './PronounsUser';
 import { Language } from './Language';
 import { PronounsProvider } from './PronounsProvider';
 import { getPronounsBadge } from './PronounsBadge';
 var users = new Map();
 
-export async function getUser(username:string, language:Language = Language.en):Promise<PronounsPageUser>{
+export async function getUser(username:string, language:Language = Language.en):Promise<PronounsUser>{
     return newUser(username, language, PronounsProvider.pronounsPage);
 }
-export async function getPronounsPageUser(username:string, language:Language = Language.en):Promise<PronounsPageUser>{
+export async function getPronounsPageUser(username:string, language:Language = Language.en):Promise<PronounsUser>{
     return getUser(username,language);
 }
 export async function getPronounsOfUser(username:string, language:Language = Language.en):Promise<Array<String>>{
-    const p:PronounsPageUser = await getUser(username, language);
+    const p:PronounsUser = await getUser(username, language);
     return p.getPronounsList();
 }
 export async function getAgeOfUser(username:string):Promise<Number>{
-    const p:PronounsPageUser = await getUser(username, Language.en);
+    const p:PronounsUser = await getUser(username, Language.en);
     return await p.getAge();
 }
 export async function getFormattedPronounsOfUser(username:string, language:Language = Language.en):Promise<String>{
-    const p:PronounsPageUser = await getUser(username, language);
+    const p:PronounsUser = await getUser(username, language);
     return (await p.getPronounsList()).join(', ');
 } 
 export async function getHTMLFormattedPronounsOfUser(username:string, language:Language = Language.en):Promise<HTMLSpanElement>{
-    const p:PronounsPageUser = await getUser(username, language);
+    const p:PronounsUser = await getUser(username, language);
     return (await p.getHTMLFormattedPronouns(true));
 }
 export async function getHTMLFormattedPronounsOfUserNoLink(username:string, language:Language = Language.en):Promise<HTMLSpanElement>{
-    const p:PronounsPageUser = await getUser(username, language);
+    const p:PronounsUser = await getUser(username, language);
     return (await p.getHTMLFormattedPronouns(false));
 }
 export async function getPreferedNamesOfUser(username:string, language:Language = Language.en):Promise<Array<String>>{
-    const p:PronounsPageUser = await getUser(username, language);
+    const p:PronounsUser = await getUser(username, language);
     return p.getNamesList(1);
 }
 export async function getAllNamesOfUser(username:string, language:Language = Language.en, minimumOpinion:number = -1):Promise<Array<String>>{
-    const p:PronounsPageUser = await getUser(username, language);
+    const p:PronounsUser = await getUser(username, language);
     return p.getNamesList(minimumOpinion);
 }
 export async function getHTMLFormattedNamesOfUser(username:string, language:Language = Language.en, minimumOpinion:number = 0):Promise<HTMLElement>{
-    const p:PronounsPageUser = await getUser(username, language);
+    const p:PronounsUser = await getUser(username, language);
     var retVal:HTMLElement = document.createElement('span');
     var counter = 0;
     for (const name of p.getNamesList(minimumOpinion)) {
@@ -63,6 +63,6 @@ export async function getHTMLFormattedNamesOfUser(username:string, language:Lang
     return retVal;
 }
 export async function getPronounsBadgeOfUser(username:string, language:Language = Language.en):Promise<HTMLImageElement>{
-    const p:PronounsPageUser = users.get(username);
+    const p:PronounsUser = users.get(username);
     return getPronounsBadge(await p.getPronounsList());
 }
