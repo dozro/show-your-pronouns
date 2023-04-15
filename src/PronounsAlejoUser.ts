902e6e1d8bc3ced403bfecff6b303eacc1a2bc8d
Julian <gitea+julian@ic.thejulian.uk>
2023-04-15 18:16:39 +0200

    various updates

--- src/PronounsAlejoUser.ts
+++ src/PronounsAlejoUser.ts
@@ -6,4 +6,53 @@ export class PronounsAlejoUser extends PronounsUser {
         retVal.push(this.data[0].name);
         return retVal;
     }
+    /**
+     * This function returns a numerical value based on the input pronoun, either from a predefined
+     * provider or from a JSON object.
+     * 
+     * @param pronoun The pronoun parameter is a string that represents the pronoun for which the
+     * function will return an opinion.
+     * 
+     * @return a number, which is either 1 or the value of the pronoun in the JSON data.
+     */
+    public getOpinionOnPronouns(pronoun:String):Number{
+        // @ts-ignore
+        retVal.push(this.data[0].pronoun_id);
+        return 1;
+    
+    }
+    public getPrideFlags():Array<string>{
+        var retVal:Array<string> = new Array();
+        var raw:JSON;
+        raw = eval('this.data.profiles.' + this.language + '.flags');
+        for (const flag in raw) {
+            // @ts-ignore
+            retVal.push(raw[flag]);
+        }
+        return retVal;
+    }
+    public async getPronounsList(minimumOpinion:number = 0):Promise<string[]>{
+        var retVal:Array<string> = new Array();
+        // @ts-ignore
+        retVal.push(await (this.getData()[0]).pronoun_id);
+        return retVal;
+    }
+    public async getAge():Promise<Number>{
+        throw new Error('PronounsAlejo does not support age');
+    }
+    public async fetchPronouns():Promise<void>{
+        var response;
+        response = await fetch('https://pronouns.alejo.io/api/users/' + this.username);
+        this.data = await response.json();
+        if (response.status == 404){
+            this.data = null;
+            throw new Error('User not found');
+        }
+    }
+    public async getAvatar():Promise<URL>{
+        var retVal:URL;
+        // @ts-ignore
+        retVal = new URL(await (this.getData()).avatar);
+        return retVal;
+    }
 }
--- src/UserMng.ts
+++ src/UserMng.ts
@@ -1,13 +1,14 @@
 import {PronounsUser} from "./PronounsUser";
 import {Language} from "./Language";
 import {PronounsProvider} from "./PronounsProvider";
+import {getPronounsUser} from "./PronounsUserFactory";
 export var users = new Map();
 export async function newUser(username:string, language:Language = Language.en, provider:PronounsProvider = PronounsProvider.pronounsPage):Promise<PronounsUser>{
     if(users.has(username)){
         users.get(username).setLanguage(language);
         return users.get(username);
     } else {
-        users.set(username, new PronounsUser(username, language,provider));
+        users.set(username, getPronounsUser(provider, username, language));
         await users.get(username).fetchPronouns();
         return users.get(username);
     }
