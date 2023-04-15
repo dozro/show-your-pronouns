007813d4012ae013561b82dc5c76f4ac6d7de2a5
Julian <gitea+julian@ic.thejulian.uk>
2023-04-15 18:20:55 +0200

    commented out faulty import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as _ from 'lodash';
+//import * as _ from 'lodash';
 import { Language } from './Language';
 import { PronounsProvider } from './PronounsProvider';
 import {getUser, getAgeOfUser, getAllNamesOfUser, getFormattedPronounsOfUser, getHTMLFormattedNamesOfUser, getHTMLFormattedPronounsOfUser, getPreferedNamesOfUser, getPronounsBadgeOfUser, getPronounsOfUser, getHTMLFormattedPronounsOfUserNoLink, getPronounsPageUser, getPrideFlagsOfUser} from './HTMLAdapter';
